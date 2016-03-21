7e7b83c3e958f477251729c6fcc6b1a09b3d1799
Kevin Roark <ker2143@columbia.edu>
2016-03-21 11:50:43 -0400

    hook up basic slider to rate

--- score.js
+++ score.js
@@ -11,7 +11,12 @@ var track = frampton.util.choice(mediaConfig.audio);
 var audioSegment = new frampton.AudioSegment(track);
 audioSegment.loop = true;
 
+var rateInput = document.querySelector('#rate-input');
+rateInput.onchange = function() {
+  var rate = rateInput.value;
+  colorSegment.setPlaybackRate(rate);
+  audioSegment.setPlaybackRate(rate);
+};
+
 renderer.scheduleSegmentRender(colorSegment, 2000);
 renderer.scheduleSegmentRender(audioSegment, 2000);
-
-console.log(colorSegment);
