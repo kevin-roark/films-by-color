9ed78562e23d81f46e8d4f32bf779d7d20740bd5
Kevin Roark <ker2143@columbia.edu>
2016-03-22 00:16:41 -0400

    log slider

--- score.js
+++ score.js
@@ -1,4 +1,25 @@
 
+/// Helpful lil guy http://stackoverflow.com/questions/846221/logarithmic-slider
+function LogSlider(options) {
+  if (!options) options = {};
+  this.minpos = options.minpos || 0;
+  this.maxpos = options.maxpos || 100;
+  this.minlval = Math.log(options.minval || 1);
+  this.maxlval = Math.log(options.maxval || 100000);
+
+  this.scale = (this.maxlval - this.minlval) / (this.maxpos - this.minpos);
+}
+
+LogSlider.prototype.value = function(position) {
+  return Math.exp((position - this.minpos) * this.scale + this.minlval);
+};
+
+LogSlider.prototype.position = function(value) {
+  return this.minpos + (Math.log(value) - this.minlval) / this.scale;
+};
+
+/// Score
+
 var renderer = new frampton.Renderer({
   mediaConfig: mediaConfig
 });
@@ -11,9 +32,21 @@ var track = frampton.util.choice(mediaConfig.audio);
 var audioSegment = new frampton.AudioSegment(track);
 audioSegment.loop = true;
 
+var logSlider = new LogSlider({
+  minval: 0.05,
+  maxval: 100
+});
+
+var playbackRateEl = document.querySelector('.playback-rate');
+
 var rateInput = document.querySelector('#rate-input');
-rateInput.onchange = function() {
-  var rate = rateInput.value;
+rateInput.value = logSlider.position(1.0);
+
+rateInput.onchange = rateInput.oninput = function() {
+  var rate = logSlider.value(rateInput.value);
+
+  playbackRateEl.textContent = (Math.round(rate * 100) / 100) + 'x';
+
   colorSegment.setPlaybackRate(rate);
   audioSegment.setPlaybackRate(rate);
 };
