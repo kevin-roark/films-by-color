cc8ac4cff1169159f8066be821b93fa1c08065c9
Kevin Roark <ker2143@columbia.edu>
2016-03-23 03:27:25 -0400

    home page

---
+++ js/home.js
@@ -0,0 +1,79 @@
+
+(function() {
+
+  var colorSliceMap = {
+    'red-color-link': {
+      title: 'Red',
+      color: 'red',
+      img: 'media/color-slices/red.png'
+    },
+    'orange-color-link': {
+      title: 'Mad Max',
+      color: 'orange',
+      img: 'media/color-slices/orange.png'
+    },
+    'yellow-color-link': {
+      title: 'Minions',
+      color: 'yellow',
+      img: 'media/color-slices/yellow.png'
+    },
+    'green-color-link': {
+      title: 'Shrek',
+      color: 'green',
+      img: 'media/color-slices/green.png'
+    },
+    'blue-color-link': {
+      title: 'Deep Blue Sea',
+      color: 'blue',
+      img: 'media/color-slices/blue.png'
+    },
+    'indigo-color-link': {
+      title: 'Spring Breakers',
+      color: 'indigo',
+      img: 'media/color-slices/indigo.png'
+    },
+    'violet-color-link': {
+      title: 'Raw Nerves: A Lacanian Thriller',
+      color: 'violet',
+      img: 'media/color-slices/violet.png'
+    }
+  };
+
+  var linkTitleEl = document.querySelector('.link-title');
+
+  var colorLinks = document.querySelectorAll('.color-link');
+  for (var i = 0; i < colorLinks.length; i++) {
+    hoverify(colorLinks[i]);
+  }
+
+  function hoverify(colorLink) {
+    var colorInfo = colorSliceMap[colorLink.id];
+
+    var img = document.createElement('img');
+    img.src = colorInfo.img;
+    img.style.width = '100%'; img.style.height = '100%';
+    img.style.opacity = 0.75;
+    img.style.display = 'none';
+    colorLink.appendChild(img);
+
+    var isOver = false;
+
+    colorLink.onmouseover = function() {
+      if (!isOver) {
+        img.style.display = 'block';
+        linkTitleEl.style.color = colorInfo.color;
+        linkTitleEl.textContent = colorInfo.title;
+        isOver = true;
+      }
+    };
+
+    colorLink.onmouseout = function() {
+      if (isOver) {
+        img.style.display = 'none';
+        linkTitleEl.textContent = '';
+        isOver = false;
+      }
+    };
+  }
+
+})();
